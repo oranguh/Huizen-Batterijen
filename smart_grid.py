be791ae086e8fb61b6206c0a620b3db06775c260
oranguh <makoheuvelman@hotmail.com>
2018-04-12 16:36:18 +0200

    some typo

--- smart_grid.py
+++ smart_grid.py
@@ -15,14 +15,14 @@ class smart_grid():
 
     def create_house(self, position, output):
         """ creates house object at position with output"""
-        position = self.position
-        output = self.output
+        self.position = position
+        self.output = output
 
-        
+
     def create_battery(self, position, capacity):
         """ creates battery object at position with capacity"""
-        position = self.position
-        capacity = self.capacity
+        self.position = position
+        self.capacity = capacity
 
     def check_validity():
         """ Checks whether the smart_grid is fully connected """
