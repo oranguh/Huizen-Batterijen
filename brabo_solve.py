2488c0b83ae9295387cce7f547cce58894bb7875
niels <niels_v_opstal@live.nl>
2018-04-25 14:20:54 +0200

    eerste errors gefixt

--- brabo_solve.py
+++ brabo_solve.py
@@ -1,50 +1,43 @@
-    """
-    1) Maak ‘route-klasse’ maar geen stack
-    2) vulVolgendePlaatsIn(Route p)
-    3) Maak alle kinderen k1 ... k13 (of minder)
-    4) Voor alle kx: controleer if laatste plaats.
-    4a) Ja? Stop en bereken lengte. (beter? bewaren!)
-    4b) Nee? vulVolgendePlaatsIn(p)
-    5) Klaar? Bewaarde route = best
-    """
-
 class node:
 
-    def __init__(self, houseNumber = 0, batterydict, houseDict, subPrice = 0, bestPrice):
+    def __init__(self, batteryDict, houseDict, bestPrice, subPrice = 0,houseNumber = 0):
         self.houseNumber = houseNumber
         self.lastNode = False
-        self.subPrice = price
+        self.subPrice = subPrice
         self.batteries = batteryDict
         self.houses = houseDict
         self.bestPrice = bestPrice
         # battery dict = pos, capacity_left
         #house dict = pos, output, connected_to
 
-    def solve(self, bestPrice):
+    def solve(self):
         for i, battery in enumerate(self.batteries):
-            if self.houses[houseNumber]['output'] < battery['capacity_left']
-                diff_x = abs(self.houses[houseNumber]['position'][0] - battery['position'][0])
-                diff_y = abs(self.houses[houseNumber]['position'][1] - battery['position'][1])
-                nextSubPrice += ((diff_x + diff_y) * 9)
-                nextHouseNumber = self.houseNumber + 1
-                battery['capacity_left'] -= houses[houseNumber]['output']
-                self.houses[houseNumber]['connected_to'] = battery['position']
-                newNode = node(nextHouseNumber, self.batteries, self.houses, nextSubPrice)
-                # Hier is dus de laatste geconnect
-                if nextHouseNumber is len(self.houses
-                    # Is dit de beste oplossing tot nu toe?
-                    if nextSubPrice < bestPrice:
-                    with open("Best_brabo_solution", "w") as f:
-                        writer = csv.writer(f)
-                        writer.writerow(["score", "configuration"])
-                        writer.writerow([nextSubPrice, houses])
-                        return nextSubPrice
+            print("1")
+            if self.houses[self.houseNumber]['output'] < battery['capacity']:
+                print("2")
+                diff_x = abs(self.houses[self.houseNumber]['position'][0] - battery['position'][0])
+                diff_y = abs(self.houses[self.houseNumber]['position'][1] - battery['position'][1])
 
-                elif nextSubPrice < bestPrice:
-                    self.bestPrice = newnode.solve()
+                nextSubPrice = self.subPrice + ((diff_x + diff_y) * 9)
+                nextHouseNumber = self.houseNumber + 1
+                battery['capacity'] -= self.houses[self.houseNumber]['output']
+                self.houses[self.houseNumber]['connected_to'] = battery['position']
+                newNode = node(self.batteries, self.houses, nextSubPrice, nextHouseNumber)
+                # Hier is dus de laatste geconnect
+                if nextHouseNumber is len(self.houses):
+                    # Is dit de beste oplossing tot nu toe?
+                    if (nextSubPrice < self.bestPrice):
+                        with open("Best_brabo_solution", "w") as f:
+                            writer = csv.writer(f)
+                            writer.writerow(["score", "configuration"])
+                            writer.writerow([nextSubPrice, houses])
+                            return nextSubPrice
+
+                elif nextSubPrice < self.bestPrice:
+                    self.bestPrice = newNode.solve()
                 else:
                     return self.bestPrice
-            elif battery['capacity_left'] < 20: #willen we dit 20?
+            elif battery['capacity'] < 20: #willen we dit 20?
                 # Delete de batterij uit de node als er minder dan 20 capacity over is
                 del self.batteries[i]
             else:
