7db5cf7632a72c4939cc27fdc8be6e0c0be9260e
niels <niels_v_opstal@live.nl>
2018-04-26 10:32:20 +0200

    Bezig met debuggen

--- brabo_solve.py
+++ brabo_solve.py
@@ -2,7 +2,6 @@ class node:
 
     def __init__(self, batteryDict, houseDict, bestPrice, subPrice = 0,houseNumber = 0):
         self.houseNumber = houseNumber
-        self.lastNode = False
         self.subPrice = subPrice
         self.batteries = batteryDict
         self.houses = houseDict
@@ -11,17 +10,23 @@ class node:
         #house dict = pos, output, connected_to
 
     def solve(self):
+
         for i, battery in enumerate(self.batteries):
             print("1")
+
+            # If kan connecten
             if self.houses[self.houseNumber]['output'] < battery['capacity']:
                 print("2")
                 diff_x = abs(self.houses[self.houseNumber]['position'][0] - battery['position'][0])
                 diff_y = abs(self.houses[self.houseNumber]['position'][1] - battery['position'][1])
-
+                print(diff_x)
+                print(diff_y)
                 nextSubPrice = self.subPrice + ((diff_x + diff_y) * 9)
+                print(nextSubPrice)
                 nextHouseNumber = self.houseNumber + 1
                 battery['capacity'] -= self.houses[self.houseNumber]['output']
                 self.houses[self.houseNumber]['connected_to'] = battery['position']
+
                 newNode = node(self.batteries, self.houses, nextSubPrice, nextHouseNumber)
                 # Hier is dus de laatste geconnect
                 if nextHouseNumber is len(self.houses):
@@ -31,15 +36,25 @@ class node:
                             writer = csv.writer(f)
                             writer.writerow(["score", "configuration"])
                             writer.writerow([nextSubPrice, houses])
-                            return nextSubPrice
+                        return nextSubPrice
+                    else:
+                        return self.bestPrice
+
 
                 elif nextSubPrice < self.bestPrice:
+                    print("HALLO")
                     self.bestPrice = newNode.solve()
+                elif nextSubPrice > self.bestPrice:
+                    print("DOEI")
                 else:
                     return self.bestPrice
+                print("5")
+            #if niet kan connecten
             elif battery['capacity'] < 20: #willen we dit 20?
                 # Delete de batterij uit de node als er minder dan 20 capacity over is
+                print("3")
                 del self.batteries[i]
             else:
+                print("4")
                 continue
         return self.bestPrice
