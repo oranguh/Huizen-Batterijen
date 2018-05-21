255e24c2c84255f0ff7f17ec4be8250bc55637b3
niels <niels_v_opstal@live.nl>
2018-05-21 15:26:14 +0200

    laatste commitje v/d dag

--- controle.py
+++ controle.py
@@ -4,6 +4,7 @@ import colorama
 from termcolor import cprint
 import matplotlib.pyplot as plt
 
+
 sys.path.append('Code/Helper_Functions')
 sys.path.append('Code/Algorithms')
 sys.path.append('Data/')
@@ -18,6 +19,7 @@ from solution_reader_new_format import solution_reader as solution_reader_new
 
 house_path = 'Data/wijk1_huizen.csv'
 battery_path = 'Data/wijk1_batterijen.txt'
+battery_path = 'Results/Battery_configurations/SCORE_4486_SIGMA_10.csv'
 
 houses, batteries = read_data(house_path, battery_path)
 
@@ -45,7 +47,7 @@ solution_reader_new(wijk1, 'Results/best_hillclimber.json')
 # print(wijk1.house_dict_with_manhattan_distances)
 
 wijk1.connect_from_new_structure(wijk1.house_dict_with_manhattan_distances)
-
+heat_map(wijk1)
 wijk1.cap_left()
 print(wijk1.calc_cost())
 print(wijk1.check_validity())
