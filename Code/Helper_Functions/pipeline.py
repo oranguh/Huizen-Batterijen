ed7cc421395b503b5c13b7c32026ded653f6e3b4
niels <niels_v_opstal@live.nl>
2018-05-24 12:29:58 +0200

    weer stappen met de pipeline

--- Code/Helper_Functions/pipeline.py
+++ Code/Helper_Functions/pipeline.py
@@ -2,6 +2,7 @@ import json
 from operator import itemgetter
 import sys
 from battery_placer_for_pipeline import battery_placer
+from smart_grid import SmartGrid
 
 sys.path.append('../Algorithms')
 from random_solve import random_solve
@@ -17,9 +18,21 @@ def main():
     best_4_bat_configs = sorted(parsed_data["ALL_CONFIGURATIONS"], key=itemgetter('score'))[0:4]
 
     for comp in best_4_bat_configs:
-        print(comp)
-        comp = battery_placer(create_house_dict(1), comp, 10)
-        print(comp)
+        houses = create_house_dict(1)
+        comp = battery_placer(houses, comp, 10)
+        compwijk = SmartGrid(51,51)
+        battery_dict = []
+        for element in houses:
+            compwijk.create_house(element['position'], element['output'])
+        for i,element in enumerate(comp["batteries"]):
+            compwijk.create_battery(comp['bat_positions'][i], element)
+            battery_dict.append({"position" : comp["bat_positions"][i], "capacity" : element})
+        compwijk.add_house_dictionaries(houses)
+        compwijk.add_battery_dictionaries(battery_dict)
+        compwijk.grid = random_solve(compwijk)
+        compwijk.prettify()
+
+
 
 
 def create_house_dict(wijk):
